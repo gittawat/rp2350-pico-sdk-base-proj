95988ff27e7b954298bcb89193e47634b5306a12
gittawat <kitawat49254@gmail.com>
2026-06-29 22:37:56 +0700

    init

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "pico/stdlib.h"
+
+int main() {
+  // LD2 (Green LED) is tied directly to GPIO25 on the W6300-EVB-Pico2
+  const uint LED_PIN = 25;
+
+  // Initialize the chosen GPIO pin
+  gpio_init(LED_PIN);
+
+  // Set the pin direction to output
+  gpio_set_dir(LED_PIN, GPIO_OUT);
+
+  // Infinite loop to toggle the LED status
+  while (true) {
+    gpio_put(LED_PIN, 1); // Turn the LED on
+    sleep_ms(250);        // Delay for 500 milliseconds
+    gpio_put(LED_PIN, 0); // Turn the LED off
+    sleep_ms(250);        // Delay for 500 milliseconds
+  }
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+pico-sdk rp2350 template project
+
+building:
+    cd build
+    cmake -DCMAKE_BUILD_TYPE=Debug ..
+    cmake --build .
+
+todo:
+- setup proper .clangd
+- setup proper debug and release directory 
